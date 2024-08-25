94812ca1ef4008cf38d74602e678021ae7c58577
Abhishek <abhishekbmarathe@gmail.com>
2024-08-25 22:43:51 +0530

    update mods done

--- src/components/Ticket_Creation/Openticket.jsx
+++ src/components/Ticket_Creation/Openticket.jsx
@@ -96,28 +96,89 @@ const NewTicket = () => {
     );
   };
 
+
+
+
+  const [users, setUsers] = useState([]); // Collaborators
+  const [ticketAss, setAssign] = useState([]); // Technicians
+
+  useEffect(() => {
+    const data = localStorage.getItem("onlyUsers");
+    if (data) {
+      const parsedData = JSON.parse(data);
+      const adminUsers = parsedData.filter((item) => item.role === "admin").map((item) => item.username);
+      const technicianUsers = parsedData.filter((item) => item.role === "technician").map((item) => item.username);
+
+      setUsers(adminUsers);
+      setAssign(technicianUsers);
+    }
+  }, []);
+
+
+
+
+  const [helpTopics, setHelpTopics] = useState([]);
+  const [departments, setDepartments] = useState([]);
+  const [slaPlans, setSlaPlans] = useState([]);
+  const [cannedResponse, setCannedResponses] = useState([]);
+  const [creator, setCreator] = useState(JSON.parse(localStorage.getItem('userDet')).username);
+  useEffect(() => {
+    const storedSettings = localStorage.getItem("TicketSettings");
+    if (storedSettings) {
+      try {
+        const parsedSettings = JSON.parse(storedSettings);
+
+        if (Array.isArray(parsedSettings)) {
+          // Filter and set the settings based on type
+          setHelpTopics(parsedSettings.filter(setting => setting.type === 'Help topic').map(setting => setting.data));
+          setDepartments(parsedSettings.filter(setting => setting.type === 'Department').map(setting => setting.data));
+          setSlaPlans(parsedSettings.filter(setting => setting.type === 'SLA Plan').map(setting => setting.data));
+          setCannedResponses(parsedSettings.filter(setting => setting.type === 'Canned Responses').map(setting => setting.data));
+        }
+      } catch (error) {
+        console.error("Failed to parse settings from localStorage:", error);
+      }
+    }
+  }, []);
+
+
+
+
+
   const onSubmit = (data) => {
-    if (allowEdit) {
+    if (!allowEdit) {
+      // Get current date and time
+      const currentDateTime = new Date();
+
+      // Format the date as dd-mm-yyyy hh:mm:ss
+      const formattedDate = `${String(currentDateTime.getDate()).padStart(2, '0')}/${String(currentDateTime.getMonth() + 1).padStart(2, '0')}/${currentDateTime.getFullYear()} ${String(currentDateTime.getHours()).padStart(2, '0')}:${String(currentDateTime.getMinutes()).padStart(2, '0')}:${String(currentDateTime.getSeconds()).padStart(2, '0')}`;
       const formData = {
         ...data,
         contactDetails: contacts,
         collaborators: selectedUsers,
         companyType,
         ticketStatus,
-        updatedDate: new Date() // Only the date portion
+        ticketNumber,
+        updatedDate: formattedDate // Only the date portion
       };
+
+      console.log(formData);
 
       axios
         .post('https://binarysystemsbackend-mtt8.onrender.com/api/updateTicket', formData)
         .then((response) => {
-          alert('Ticket updated Successfully');
+          alert('Ticket updated successfully');
+          console.log(response);
+          // Instead of reloading the page, consider navigating to another page or updating the state
           window.location.reload();
+          navigate(-1); // Replace '/some-page' with the actual route you want to navigate to
         })
         .catch((error) => {
-          console.error('Error Updating ticket:', error);
+          console.error('Error updating ticket:', error);
         });
     }
   };
+
 
   const handleDelete = () => {
     const conf = confirm("Are sure to delete this ticket?");
@@ -158,10 +219,10 @@ const NewTicket = () => {
           <div className={`w-1/3 text-center pb-2 ${step === 1 ? 'border-b-4 border-blue-600 text-blue-600' : 'text-gray-500'}`}>
             Customer Info
           </div>
-          <div className={`w-1/3 text-center pb-2 ${step === 2 ? 'border-b-4 border-blue-600 text-blue-600' : 'text-gray-500'}`}>
+          <div className={`w-1/3 text-center pb-2 ${step === 2 || step === 3 ? 'border-b-4 border-blue-600 text-blue-600' : 'text-gray-500'}`}>
             Ticket Info
           </div>
-          <div className={`w-1/3 text-center pb-2 ${step === 3 ? 'border-b-4 border-blue-600 text-blue-600' : 'text-gray-500'}`}>
+          <div className={`w-1/3 text-center pb-2 ${step === 4 ? 'border-b-4 border-blue-600 text-blue-600' : 'text-gray-500'}`}>
             Response
           </div>
         </div>
@@ -302,49 +363,41 @@ const NewTicket = () => {
             </div>
 
             {/* Help Topic */}
-            <div className="mb-6">
-              <label htmlFor="helpTopic" className="block text-lg font-medium mb-2">
-                Help Topic
-              </label>
-              <input
-                type="text"
-                id="helpTopic"
-                {...register('helpTopic', { required: true })}
-                className="w-full p-3 border border-gray-300 rounded"
-                readOnly={allowEdit}
-              />
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="helpTopic">Help Topic</label>
+              <select className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('helpTopic')} id="helpTopic" readOnly={allowEdit}>
+                <option value="">Select Help Topic</option>
+                {helpTopics.map((topic, index) => (
+                  <option key={index} value={topic}>{topic}</option>
+                ))}
+              </select>
             </div>
 
             {/* Department */}
-            <div className="mb-6">
-              <label htmlFor="department" className="block text-lg font-medium mb-2">
-                Department
-              </label>
-              <input
-                type="text"
-                id="department"
-                {...register('department', { required: true })}
-                className="w-full p-3 border border-gray-300 rounded"
-                readOnly={allowEdit}
-              />
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="department">Department</label>
+              <select className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('department')} id="department" readOnly={allowEdit}>
+                <option value="">Select Department</option>
+                {departments.map((department, index) => (
+                  <option key={index} value={department}>{department}</option>
+                ))}
+              </select>
             </div>
 
             {/* SLA Plan */}
-            <div className="mb-6">
-              <label htmlFor="slaPlan" className="block text-lg font-medium mb-2">
-                SLA Plan
-              </label>
-              <input
-                type="text"
-                id="slaPlan"
-                {...register('slaPlan', { required: true })}
-                className="w-full p-3 border border-gray-300 rounded"
-                readOnly={allowEdit}
-              />
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="slaPlan">SLA</label>
+              <select className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('slaPlan')} id="slaPlan"
+                readOnly={allowEdit}>
+                <option value="">Select SLA</option>
+                {slaPlans.map((sla, index) => (
+                  <option key={index} value={sla}>{sla}</option>
+                ))}
+              </select>
             </div>
 
             {/* Due Date */}
-            <div className="mb-6">
+            {/* <div className="mb-6">
               <label htmlFor="dueDate" className="block text-lg font-medium mb-2">
                 Due Date
               </label>
@@ -355,22 +408,10 @@ const NewTicket = () => {
                 className="w-full p-3 border border-gray-300 rounded"
                 readOnly={allowEdit}
               />
-            </div>
+            </div> */}
 
             {/* Assigned To */}
-            <div className="mb-6">
-              <label htmlFor="assignedTo" className="block text-lg font-medium mb-2">
-                Assigned To
-              </label>
-              <input
-                type="text"
-                id="assignedTo"
-                {...register('assignedTo')}
-                className="w-full p-3 border border-gray-300 rounded"
-                readOnly={allowEdit}
-                onChange={handleAssignedToChange}
-              />
-            </div>
+
 
             <div className="flex justify-between">
               <button
@@ -390,54 +431,85 @@ const NewTicket = () => {
             </div>
           </div>
         )}
-
-        {/* Step 3: Response */}
         {step === 3 && (
-          <div>
-            {/* Canned Response */}
-            <div className="mb-6">
-              <label htmlFor="cannedResponse" className="block text-lg font-medium mb-2">
-                Canned Response
-              </label>
-              <textarea
-                id="cannedResponse"
-                {...register('cannedResponse')}
-                className="w-full p-3 border border-gray-300 rounded"
-                rows="5"
-                readOnly={allowEdit}
-              />
-            </div>
-
-            {/* Issue Description */}
-            <div className="mb-6">
-              <label htmlFor="issueDescription" className="block text-lg font-medium mb-2">
-                Issue Description
-              </label>
-              <textarea
-                id="issueDescription"
-                {...register('issueDescription', { required: true })}
-                className="w-full p-3 border border-gray-300 rounded"
-                rows="5"
-                readOnly={allowEdit}
-              />
-            </div>
-
+          <div className="flex flex-col gap-6">
             {/* Priority */}
-            <div className="mb-6">
-              <label htmlFor="priority" className="block text-lg font-medium mb-2">
-                Priority
+            <div className="mb-4">
+              <label className="block mb-2 text-lg font-medium" htmlFor="priority">Priority</label>
+              <div className="flex gap-3 items-center">
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    value="Low"
+                    {...register('priority')}
+                    className="form-radio text-blue-600"
+                    readOnly={allowEdit}
+                  />
+                  <span className="ml-2">Low</span>
+                </label>
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    value="Normal"
+                    {...register('priority')}
+                    className="form-radio text-blue-600"
+                    readOnly={allowEdit}
+                  />
+                  <span className="ml-2">Normal</span>
+                </label>
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    value="High"
+                    {...register('priority')}
+                    className="form-radio text-blue-600"
+                    readOnly={allowEdit}
+                  />
+                  <span className="ml-2">High</span>
+                </label>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="assignedTo" className="block   mb-2 text-lg font-medium">
+                Assigned To
               </label>
-              <select
-                id="priority"
-                {...register('priority', { required: true })}
+              <input
+                type="text"
+                id="assignedTo"
+                {...register('assignedTo')}
                 className="w-full p-3 border border-gray-300 rounded"
-                disabled={allowEdit}
+                readOnly={allowEdit}
+                onChange={handleAssignedToChange}
+              />
+            </div>
+            <div className="mb-4 relative">
+              <label className="block mb-2 text-lg font-medium" htmlFor="collaborators">Collaborators</label>
+              <div
+                className="w-full p-2 bg-transparent border border-gray-600 rounded cursor-pointer"
+                onClick={toggleDropdown}
               >
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-                <option value="Urgent">Urgent</option>
-              </select>
+                {selectedUsers.length > 0 ? selectedUsers.join(", ") : "Select Collaborators"}
+              </div>
+
+              {isDropdownOpen && (
+                <div className="absolute mt-2 w-full bg-white border border-gray-600 rounded shadow-lg z-10">
+                  {users.map((user, index) => (
+                    <div key={index} className="px-4 py-2 flex items-center">
+                      <input
+                        type="checkbox"
+                        id={`user-${index}`}
+                        checked={selectedUsers.includes(user)}
+                        onChange={() => handleUserSelection(user)}
+                        className="mr-2"
+                      />
+                      <label htmlFor={`user-${index}`} className="text-sm">
+                        {user}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+
+              )}
             </div>
 
             <div className="flex justify-between">
@@ -448,13 +520,57 @@ const NewTicket = () => {
               >
                 Previous
               </button>
+              <button
+                type="button"
+                className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700"
+                onClick={() => setStep(4)}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+
+        {/* Step 3: Response */}
+        {step === 4 && (
+          <div>
+            {/* Canned Response */}
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="cannedResponse">Canned Response</label>
+              <select className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('cannedResponse')} id="cannedResponse">
+                <option value="">Select Canned</option>
+                {cannedResponse.map((canned, index) => (
+                  <option key={index} value={canned}>{canned}</option>
+                ))}
+                <option value="response2">Response 2</option>
+              </select>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="issueDescription">Issue Description</label>
+              <textarea className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('issueDescription')} id="issueDescription"></textarea>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm mb-2" htmlFor="">Additional Info</label>
+              <textarea className="w-full p-2 bg-transparent border border-gray-600 rounded focus:outline-none focus:border-blue-500" {...register('additionalInfo')} id=""></textarea>
+            </div>
+
+
+
+            <div className="flex justify-between">
+              <button
+                type="button"
+                className="px-6 py-3 bg-gray-300 text-gray-700 rounded hover:bg-gray-400"
+                onClick={() => setStep(3)}
+              >
+                Previous
+              </button>
               {!allowEdit && (
                 <button
                   type="submit"
                   className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700"
-                  onClick={onSubmit}
+
                 >
-                  Save
+                  update
                 </button>
               )}
             </div>
